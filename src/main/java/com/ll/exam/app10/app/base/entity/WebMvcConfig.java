4e7e0c4c1d6a536be5049694d59357fdc0add446
ghdtmdvy2 <ghdtmdvy2@gmail.com>
2022-09-06 13:51:27 +0900

    로컬 경로 변경

--- src/main/java/com/ll/exam/app10/app/base/entity/WebMvcConfig.java
+++ src/main/java/com/ll/exam/app10/app/base/entity/WebMvcConfig.java
@@ -13,7 +13,7 @@ public class WebMvcConfig implements WebMvcConfigurer {
     @Override
     public void addResourceHandlers(ResourceHandlerRegistry registry) {
         // 클라이언트의 요청 url이 /gen-file 이라고 시작 될 경우
-        registry.addResourceHandler("/gen-file/**")
+        registry.addResourceHandler("/gen/**")
                 // 해당 파일 경로에 요청을 전달한다.
                 .addResourceLocations("file:///" + genFileDirPath + "/");
     }
